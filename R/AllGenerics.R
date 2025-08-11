152b58cb383f6c4c9f85865dbfba297624bbe790
tk <tkimhofer@gmail.com>
2025-08-11 12:37:25 +0200

    resolve warning x-ref docstr

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1025,7 +1025,7 @@ get.idx <- function(range = c(1, 5), ppm) {
 #' plot(x, type = 'l'); abline(h = range(x), lty = 2)
 #' points(scRange(x, ra = c(5, 10)), type = 'l', col = 'red'); abline(h = c(5, 10), col = 'red', lty = 2)
 #'
-#' @seealso \code{\link{minmax}}
+#' @seealso [minmax()]
 #' @export
 scRange <- function(x, ra) {
   ra <- sort(ra)
